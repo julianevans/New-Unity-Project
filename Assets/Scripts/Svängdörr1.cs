f4208f5c8bce816bcf7adf6534279c65f1ea9cf3
julianevans <julian.alfred.evans@gmail.com>
2017-03-19 17:00:32 +0100

    Rigidbody
    Rogidbody på bollen, samt en snurra och en svängdörrpussel

---
+++ Assets/Scripts/Svängdörr1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Svängdörr1 : MonoBehaviour { 
+
+    void Start()
+    {
+        transform.localPosition = new Vector3(0.53F, 0.25F, -0.174F);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    { if (other.gameObject.CompareTag("Dorr"))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+    }}
+
+//transform.localPosition = (new Vector3(0.53F, 0.25F, -0.174F));
