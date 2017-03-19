9455cf94faac9b61752d97bdca298a86b7f3b065
julianevans <julian.alfred.evans@gmail.com>
2017-03-19 14:00:51 +0100

    Fest
    FEST

---
+++ Unity/Tutorial-spelet/Assets/Materials/Scripts/Spelkontrollen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Spelkontrollen : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text wintext;
+
+    private Rigidbody rb;
+    private int count;
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        wintext.text = "";
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Snurrare"))
+          
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 13)
+        {
+            wintext.text = "Grattis, du Vann!";
+       }
+    }
+}
